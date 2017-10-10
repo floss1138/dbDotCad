230d94bb0bf1cea135d3eebeb11b8da336c16016
floss1138 <floss1138@gmail.com>
2017-10-10 15:34:10 +0100

    program now loops and adds readme to watchfolder

--- perl/dx_magic9.pl
+++ perl/dx_magic9.pl
@@ -29,6 +29,9 @@ my $dx_fail = '/home/user1/dx_fail/';
 # dx attout folder [dx files conveted to attout format
 my $dx_attout = '/home/user1/dx_attout/';
 
+# dx merge folder [dxf file for metadata replacement ]
+my $dx_merge = '/home/user1/dx_merge/';
+
 # Program variables go here:
 
 # CAD version lookup table
@@ -47,20 +50,28 @@ my %cadvintage = (
 
 # print Dumper (\%cadvintage);
 
-my @folders = ( $dx_watch, $dx_pass, $dx_fail, $dx_attout );
+my @folders = ( $dx_watch, $dx_pass, $dx_fail, $dx_attout, $dx_merge );
 
 # Print welcome message & check folders exist
 
 print "\n ***  X File Magic $PROGRAM_NAME version $VERSION  ***\n";
 
-# create folders if they do not exist
+# create folders if they do not exist & add readme
 foreach (@folders) {
     print "  Checking $_ exists";
 
     #    mkdir($_) unless ( -d $_ );
     if ( !-d ) { print " - not found, so creating ...\n"; mkdir; }
     else       { print " - OK\n"; }
-}
+# Add readme to watch folder
+my $readme = $dx_watch . 'readme.txt';
+if ( !open my $README, '>', $readme ) {
+           print "\n  failed to open $readme\n";
+         }
+         else {
+             print $README "   ## dx_magic watch folder ##\n\n   Valid dxx & dxf files found in here will be processed to create attout.txt metadata files\n   in $dx_attout, with the same file name & a new .txt extension\n   The original file is then moved to $dx_pass or $dx_fail folder as appropriate\n";
+         }
+} # End of creating folders sub
 
 # Sub to read watch folder passed as argument to read_dx_atch
 
@@ -292,8 +303,14 @@ sub xparser {
     }
     else {
         print
-"\n Finished parsing $xfile \n (DXX file or DXF of unknown version)\n\n";
+"\n Finished parsing $xfile \n (DXX file or DXF of unknown version)\n";
     }
+# move parsed file to passed directory 
+
+my $passed = $dx_pass . basename($xfile);
+print " Moving to $dx_pass ... \n\n";
+move ($xfile, $passed) or croak "move of $xfile failed";
+
 
     # Clear tagcheck before next run of xparser
     %tagcheck = ();
@@ -302,7 +319,10 @@ sub xparser {
     return ( \%hof_blocks, \@tags );
 }    # End of xparser
 
-## The Program ##
+### The Program ###
+
+# loop forever with a 1 second pause between runs
+while (sleep 1) {
 
 # Read watch folder, looking for correctly named files
 my @dx_files = read_dx_watch($dx_watch);
@@ -320,7 +340,7 @@ foreach (@dx_files) {
 
     # If file is static stat check will be the same string
     if ( $stat1 eq $stat2 ) {
-        print "  passed.\n";
+        print "  OK\n";
 
         # If static, open file and check 1st line is acceptabale format
         if ( !open my $XFILE, '<', $dx ) {
@@ -403,6 +423,9 @@ foreach (@dx_files) {
     }    # if static
 
 }    # end of foreach dx_file
+
+print " \nEnd of processing, lets check the watchfolder again for anything new ...\n";
+} # end of while (sleep 1)
 
 # attout sub takes 'attout filename with path' and 'array of elements reference'
 # as the next line to write  (append)
